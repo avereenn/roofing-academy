1cdc013f33b4763f570453e4c784416c38c1b63d
avereenn <igraverin@gmail.com>
2022-08-29 19:12:50 +0300

    added page height behavior, added form handler and popup message

--- src/js/feedbackForm.js
+++ src/js/feedbackForm.js
@@ -2,10 +2,14 @@ const feedbackLinkEl = document.querySelector('.js-feedback-link');
 const feedbackFormEl = document.querySelector('.js-feedback-form');
 const formCloseEl = document.querySelector('.js-form-close');
 const feedbackInputEl = document.querySelector('.js-feedback-input');
+const submitPopupEl = document.querySelector('.js-submit-popup');
+const popupTextEl = document.querySelector('.js-popup-text');
+const popupCloseEl = document.querySelector('.js-popup-close');
 const validation = new JustValidate('.js-feedback-form');
 const maskOptions = {
   mask: '+{7}(000)000-00-00'
 };
+let isFormValid = false;
 
 IMask(feedbackInputEl, maskOptions);
 
@@ -15,12 +19,14 @@ validation.addField('#policy-confirm', [
     errorMessage: 'Это поле обязательно',
   }
 ])
-.addField('#phone-number', [
-  {
-    rule: 'required',
-    errorMessage: 'Пожалуйста, введите номер телефона',
-  }
-]);
+  .addField('#phone-number', [
+    {
+      rule: 'required',
+      errorMessage: 'Пожалуйста, введите номер телефона',
+    }
+  ])
+  .onFail(() => isFormValid = false)
+  .onSuccess(() => isFormValid = true);
 
 function onToggleFeedbackFormLinkClick(ev) {
   ev.preventDefault();
@@ -50,3 +56,34 @@ function onCloseFeebackFormClick(ev) {
 
 feedbackLinkEl.addEventListener('click', onToggleFeedbackFormLinkClick);
 window.addEventListener('click', onCloseFeebackFormClick);
+
+feedbackFormEl.addEventListener('submit', function (ev) {
+  ev.preventDefault();
+  if(!isFormValid) return;
+
+  feedbackFormEl.classList.remove('show');
+  feedbackLinkEl.setAttribute('aria-expanded', 'false');
+
+  /*
+   * try {
+   * // отправляем заявку
+   * } catch(err) {
+   * // обрабатываем ошибку, сообщаем пользователю об ошибке
+   * popupTextEl.textContent = 'Что-то пошло не так...';
+   * submitPopupEl.classList.add('show', 'danger');
+   * setTimeout(() => submitPopupEl.classList.remove('show'), 2000);
+   * return;
+   * }
+   */
+
+  popupTextEl.textContent = 'Спасибо! Ваша заявка отправлена';
+  submitPopupEl.classList.add('show', 'success');
+  const timerID = setTimeout(() => submitPopupEl.classList.remove('show'), 2000);
+
+  popupCloseEl.addEventListener('click', () => {
+    clearTimeout(timerID);
+    submitPopupEl.classList.remove('show');
+  });
+
+  feedbackFormEl.reset();
+});
